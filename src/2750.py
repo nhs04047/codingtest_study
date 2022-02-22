fdd1c9dd95a7a60ecbbc105d49663eb91d99dd46
보현 김 <nhs04047@gmail.com>
2022-02-22 13:30:23 +0900

    upload 2750

---
+++ src/2750.py
@@ -0,0 +1,21 @@
+# 2750 - 수 정렬하기
+
+def sorting(arr):
+    if len(arr) <= 1:
+        return arr
+    
+    length = len(arr) - 1
+    for i in range(length):
+        for j in range(length-i):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
+
+N = int(input())
+nums = []
+
+for i in range(N):
+    nums.append(int(input()))
+
+for i in sorting(nums):
+    print(i)
