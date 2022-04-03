c19bd0b719315476413cc6d93e4ae709011a6814
보현 김 <nhs04047@gmail.com>
2022-04-04 01:26:29 +0900

    upload 전화번호 목록

---
+++ src/programmers/전화번호 목록.py
@@ -0,0 +1,11 @@
+# 전화번호 목록
+
+def solution(phone_book):
+    phone_book.sort()
+
+    for p1, p2 in zip(phone_book, phone_book[1:]):
+        if p2.startswith(p1):
+            return False
+    return True
+
+print(solution(["12", "123", "1235", "567", "88"]))
