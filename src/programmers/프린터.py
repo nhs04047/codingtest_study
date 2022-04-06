ef9d8954f060d52f84ac1f493097e8e8504db68a
보현 김 <nhs04047@gmail.com>
2022-04-07 00:14:40 +0900

    upload '프린터'

---
+++ src/programmers/프린터.py
@@ -0,0 +1,24 @@
+# 프린터
+
+from collections import deque
+
+def solution(priorities, location):
+    answer = 0
+    d = deque([(v,i) for i,v in enumerate(priorities)])
+    print(d)
+    while d:
+        nowlist = d.popleft()
+        if len(d)!=0:
+    
+            if nowlist[0] < max(d)[0]:
+                d.append(nowlist)
+            else:
+                answer += 1
+                if nowlist[1] == location:
+                    break
+        else:
+            answer += 1
+            if nowlist[1] == location:
+                break
+                
+    return answer
