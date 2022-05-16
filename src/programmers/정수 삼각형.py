5ac92fe86ba2972f8c413a58f1775df29153092e
보현 김 <nhs04047@gmail.com>
2022-05-17 03:41:03 +0900

    정수 삼각형 upload

---
+++ src/programmers/정수 삼각형.py
@@ -0,0 +1,19 @@
+'''
+정수 삼각형
+https://programmers.co.kr/learn/courses/30/lessons/43105
+'''
+
+def solution(triangle):
+    answer = 0
+    for i in range(1, len(triangle)):
+        for j in range(len(triangle[i])):
+            if j == 0:
+                triangle[i][j] += triangle[i-1][j]
+            elif j == i:
+                triangle[i][j] += triangle[i-1][j-1]
+            else:
+                triangle[i][j] += max(triangle[i-1][j], triangle[i-1][j-1])
+    answer = max(triangle[-1])
+    return answer
+
+print(solution([[7], [3, 8], [8, 1, 0], [2, 7, 4, 4], [4, 5, 2, 6, 5]]))
