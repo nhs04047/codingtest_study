91755a4a53c584500077392015543a61b0a135d0
보현 김 <nhs04047@gmail.com>
2022-05-24 04:09:20 +0900

    [Fix] 입국심사 수정

--- src/programmers/입국심사.py
+++ src/programmers/입국심사.py
@@ -1,24 +1,33 @@
+'''
+입국심사
+https://programmers.co.kr/learn/courses/30/lessons/43238
+'''
+'''
+이분탐색의 핵심은 '어떤 값을 이분 탐색 할건지?' 와 '기준을 무엇으로 잡아야 하는지?'임
+이 문제에서는 시간을 가지고 이분탐색을 하면서 임의의 시간에 얼마나 많은 사람을 심사 할 수 있는 지 찾아 임의의 시간을 줄여 왼쪽 기준으로, 또는 시간을 늘려 오른쪽 기준으로 넘어 갈 건지 정하면서 진행
+'''
 def solution(n, times):
     answer = 0
-    # right는 가장 비효율적으로 심사했을 때 걸리는 시간
-    # 가장 긴 심사시간이 소요되는 심사관에게 n 명 모두 심사받는 경우이다.
-    left, right = 1, max(times) * n
+    # 이진탐색 할 대상은 시간
+    # 이진 탐색을 위한 최솟값과 최댓값 설정
+    left = 0
+    right = max(times) * n
+    
     while left <= right:
-        mid = (left+ right) // 2
-        people = 0
+        # 한 심사관에게 주어지는 시간
+        mid = (left + right)//2
+        count = 0
         for time in times:
-            # people 은 모든 심사관들이 mid분 동안 심사한 사람의 수
-            people += mid // time
-            # 모든 심사관을 거치지 않아도 mid분 동안 n명 이상의 심사를 할 수 있다면 반복문을 나간다.
-            if people >= n:
+            count += mid//time
+            if count >= n:
                 break
-        
-        # 심사한 사람의 수가 심사 받아야할 사람의 수(n)보다 많거나 같은 경우
-        if people >= n:
+        # 모든 사람을 심사 할 수 있으면 시간을 줄여봄
+        if count >= n:
             answer = mid
-            right = mid - 1
-        # 심사한 사람의 수가 심사 받아야할 사람의 수(n)보다 적은 경우
-        elif people < n:
-            left = mid + 1
-            
+            right = mid-1
+        # 모슨 사람을 심사 할 수 없는 경우
+        # 한 심사관에게 주어진 시간을 늘려본다.
+        elif count < n:
+            left = mid+1
+    
     return answer
