d6bc42e7bdd4b89a8c9976897fc0cc9ad7b467dd
보현 김 <nhs04047@gmail.com>
2022-07-08 23:30:18 +0900

    add : DFS 예제

---
+++ src/이코테/DFS예제.py
@@ -0,0 +1,26 @@
+'''
+DFS예제
+'''
+def dfs(graph, v, visited):
+  visited[v] = True # 현재 노드는 방문처리
+  print(v, end=' ')
+
+  for i in graph[v]:
+    if not visited[i]:
+      dfs(graph, i, visited)
+
+graph = [
+  [],
+  [2, 3, 8],
+  [1, 7],
+  [1, 4, 5],
+  [3, 5],
+  [3, 4],
+  [7],
+  [2, 6, 8],
+  [1, 7]
+]
+
+visited = [False] * 9
+
+dfs(graph, 1, visited)
